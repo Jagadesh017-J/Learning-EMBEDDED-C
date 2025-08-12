c976e46e9579b106cda3a093370676f4c3d15a5b
Jagadesh J <jagadeshj563@gmail.com>
2025-08-12 20:14:05 +0530

    Create DELAY HEADER.h

---
+++ DELAY HEADER FILE/DELAY HEADER.h
@@ -0,0 +1,27 @@
+void delay_sec(unsigned int);
+void delay_milisec(unsigned int);
+void delay_microsec(unsigned int);
+void delay_sec(unsigned int sec)
+{
+	T0PR=15000000-1;
+	T0TCR=0X01;
+	while(T0TC<sec);
+	T0TCR=0X03;
+	T0TCR=0X00;
+
+	void delay_millisec(unsigned int sec)
+{
+	T0PR=15000-1;
+	T0TCR=0X01;
+	while(T0TC<sec);
+	T0TCR=0X03;
+	T0TCR=0X00;
+}
+void delay_microsec(unsigned int sec)
+{
+	T0PR=15-1;
+	T0TCR=0X01;
+	while(T0TC<sec);
+	T0TCR=0X03;
+	T0TCR=0X00;
+}
